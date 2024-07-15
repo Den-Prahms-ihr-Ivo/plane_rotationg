97a086de718dcb7740a8827e67a8135170b7f3de
Ivo <ivo@ivolino.de>
2024-07-15 20:37:02 +0200

    5 highlight threat angle (#7)
    * working prototype
    
    * added angle calculation
    
    * added threat elevation functionality
    
    * added azimuth visualisation
    
    * updated color scheme

--- src/plane/coordinate_system.py
+++ src/plane/coordinate_system.py
@@ -66,6 +66,18 @@ def vector_from_points(points: List[Point]):
     return np.array(_x), np.array(_y), np.array(_z)
 
 
+<<<<<<< HEAD
+=======
+def draw_line_vector(ax, x, y, z, color=[0.4, 0.46, 0.51, 0.5]):
+    arrow_prop_dict = dict(
+        mutation_scale=20, lw=1, zorder=-2, arrowstyle="-", shrinkA=0, shrinkB=0
+    )
+
+    a = Arrow3D(x, y, z, color=color, **arrow_prop_dict)
+    ax.add_artist(a)
+
+
+>>>>>>> e52da5b (5 highlight threat angle (#7))
 def draw_line(ax, point_A, point_B, color="r", lw=2):
     x, y, z = vector_from_points(np.array([point_A, point_B]))
 
